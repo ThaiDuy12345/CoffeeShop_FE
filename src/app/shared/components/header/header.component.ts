a3358d47f5df06cbf1119b6bfcf8805c4a7134fc
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-08-16 10:08:50 +0700

    update

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import Cookies from 'js-cookie';
 import { AccountData } from 'src/app/data/data';
 import { icons } from '../../utils/icon.utils';
-import { NbContextMenuComponent, NbContextMenuContext, NbContextMenuDirective, NbMenuItem } from '@nebular/theme';
+import { NbMenuService } from '@nebular/theme';
+import { filter, map } from 'rxjs';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -16,15 +19,15 @@ export class HeaderComponent implements OnInit {
       items: [
         {
           title: 'TRÀ',
-          icon: 'umbrella-outline'
+          icon: 'umbrella-outline',
         },
         {
           title: 'COFFEE',
-          icon: 'droplet-outline'
+          icon: 'droplet-outline',
         },
         {
           title: 'SẢN PHẨM GÓI',
-          icon: 'archive-outline'
+          icon: 'archive-outline',
         },
       ],
     },
@@ -34,23 +37,24 @@ export class HeaderComponent implements OnInit {
       items: [
         {
           title: 'GIỎ HÀNG',
-          icon: 'shopping-cart-outline'
+          icon: 'shopping-cart-outline',
         },
         {
           title: 'ĐƠN HÀNG BẠN ĐÃ MUA',
-          icon: 'shopping-bag-outline'
-        }
+          icon: 'shopping-bag-outline',
+        },
       ],
     },
     {
       label: 'Về chúng tôi',
       icon: icons['faHandHoldingHeart'],
-    }
+    },
   ];
   public user = {
     label: `Chúc bạn 1 ngày tốt lành`,
-    name: "",
+    name: '',
     icon: icons['faUser'],
+    tag: 'user-tag',
     items: [
       {
         title: 'THÔNG TIN',
@@ -59,12 +63,38 @@ export class HeaderComponent implements OnInit {
       {
         title: 'ĐĂNG XUẤT',
         icon: 'arrow-circle-right-outline',
-      }
-    ]
+      },
+    ],
+  };
+  constructor(
+    private nbMenuService: NbMenuService,
+    private alertService: AlertService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.setEvent();
+    const user = AccountData.find((item) => item.id === Cookies.get('id'));
+    if (user) this.user.name = user.name;
   }
 
-  ngOnInit(): void {
-    const user = AccountData.find(item => item.id === Cookies.get('id'))
-    if (user) this.user.name = user.name
+  setEvent(): void {
+    this.nbMenuService
+      .onItemClick()
+      .pipe(
+        filter(({ tag }) => tag === 'user-tag'),
+        map(({ item: { title } }) => title)
+      )
+      .subscribe((title) => {
+        if (title === 'ĐĂNG XUẤT') {
+          Cookies.remove('id');
+          setTimeout(() => {
+            this.alertService.success(
+              'Thành công',
+              `Đăng xuất thành công, Tạm biệt bạn ${this.user.name}`
+            );
+            setTimeout(() => this.router.navigate(['/sign-in']), 2000);
+          }, 1000);
+        }
+      });
   }
 }

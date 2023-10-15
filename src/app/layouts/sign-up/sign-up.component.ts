c2c317ea54ff7f0c61509711b7947987d4e752e9
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-10-15 16:26:41 +0700

    update new version

--- src/app/layouts/sign-up/sign-up.component.ts
+++ src/app/layouts/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import Cookies from 'js-cookie';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { icons } from 'src/app/shared/utils/icon.utils';
 @Component({
   selector: 'app-sign-up',
@@ -10,6 +11,7 @@ import { icons } from 'src/app/shared/utils/icon.utils';
 export class SignUpComponent implements OnInit {
   public icons = icons
   public isLoading: boolean = false
+  public isTrigger: boolean = false
   public account: {
     email: string,
     password: string,
@@ -27,7 +29,8 @@ export class SignUpComponent implements OnInit {
   }
 
   constructor(
-    private router: Router
+    private router: Router,
+    private messageService: NzMessageService
   ){}
 
   isEmailValidate(): Boolean {
@@ -36,5 +39,23 @@ export class SignUpComponent implements OnInit {
 
   isPasswordValidate(): Boolean {
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(this.account.password)
+  }
+
+  onClickSignUp(): void {
+    this.isTrigger = true
+
+    if(
+      this.account.name.length === 0 ||
+      this.account.phone.length === 0 ||
+      this.isEmailValidate() === false ||
+      this.isPasswordValidate() === false
+    ) return
+    
+    this.isLoading = true
+    setTimeout(() => {
+      this.isLoading = false
+      this.messageService.success("Đăng ký thành công, Xin vui lòng đăng nhập vào tài khoản vừa tạo")
+      this.router.navigateByUrl('/sign-in')
+    }, 3000)
   }
 }
